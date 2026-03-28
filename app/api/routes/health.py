0538ddf00d55b0cd770413289e8188837f57b9c5
Andre Ward <andre.ward62@gmail.com>
2026-03-27 22:36:57 -0300

    Projeto iniciado

---
+++ app/api/routes/health.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+@router.get("/")
+def read_root():
+    return {"status": "ok"}
+
+@router.get("/health")
+def health_check():
+    return {"service": "Projeto Apolo", "status": "running"}
